1df1dc3c06c1254918f328a28fd582eee9e6bfb7
Jake Lockitch <jakeoliver.lockitch@post.runi.ac.il>
2024-04-25 14:43:25 +0300

    started initiating the game setup, user name and gamesize

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# first commit
+gameSetup()
+
+def gameSetup():
+    userName = input("Enter your name: ")
+
+    while userName.isalpha() == False or userName == "":
+        print("Please enter a valid name")
+        userName = input("Enter your name: ")
+
+
+    # Ask the user how many coins they want to play with
+    gameSize = input(f"Hi {userName}! Enter how many coins you want to play with: ")
+
+    # Check if the input is a number
+    while gameSize.isnumeric() == False or int(gameSize) <= 0:
+        print("Please enter a positive number")
+        gameSize = input("Enter how many coins you want to play with: ")
+
+
+
+
+
+
