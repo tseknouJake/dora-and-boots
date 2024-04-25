a714fcc4b905e6b1be7d7b21a23f90b91bd8bda4
Jake Lockitch <jakeoliver.lockitch@post.runi.ac.il>
2024-04-25 16:00:23 +0300

    impletemted the coin array now need to make the choosing and do the AI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-gameSetup()
+import random
+
 
 def gameSetup():
     userName = input("Enter your name: ")
@@ -7,17 +8,32 @@ def gameSetup():
         print("Please enter a valid name")
         userName = input("Enter your name: ")
 
+    gameSize = None
 
     # Ask the user how many coins they want to play with
     gameSize = input(f"Hi {userName}! Enter how many coins you want to play with: ")
 
     # Check if the input is a number
-    while gameSize.isnumeric() == False or int(gameSize) <= 0:
+    while gameSize is None or gameSize.isnumeric() == False or int(gameSize) <= 0:
         print("Please enter a positive number")
         gameSize = input("Enter how many coins you want to play with: ")
 
+    return userName, int(gameSize)
 
 
+def initiateArray(gameSize):
+
+    coinsArr = [0] * gameSize
+
+    for i in range(gameSize):
+        random_num = random.randint(1, 100)
+        coinsArr[i] = random_num
+        print(coinsArr[i])
+
+    return coinsArr
 
 
+userName, gameSize = gameSetup()
+coinsArr = initiateArray(gameSize)
 
+print(coinsArr)
