196461a786bcbb5e08f113caf4736a390bc0f114
Jake Lockitch <jakeoliver.lockitch@post.runi.ac.il>
2024-04-25 17:10:28 +0300

    kind of implemented the playing function

--- main.py
+++ main.py
@@ -28,12 +28,47 @@ def initiateArray(gameSize):
     for i in range(gameSize):
         random_num = random.randint(1, 100)
         coinsArr[i] = random_num
-        print(coinsArr[i])
+        # print(coinsArr[i])
 
     return coinsArr
 
 
+def ArrtoStr(coinsArr):
+    coinsArr_str = [str(num) for num in coinsArr]
+    return " ".join(coinsArr_str)
+
+
 userName, gameSize = gameSetup()
 coinsArr = initiateArray(gameSize)
+coinStr = ArrtoStr(coinsArr)
 
-print(coinsArr)
+
+def playing():
+    print()
+    print("Select either the leftmost or rightmost coin")
+
+    print(ArrtoStr(coinsArr))
+
+    decision = input("Enter 'L' for leftmost or 'R' for rightmost: ")
+
+    while decision != "L" and decision != "R" and decision != "l" and decision != "r":
+        print("Please enter a valid choice")
+        decision = input("Enter 'L' for leftmost or 'R' for rightmost: ")
+
+    if decision == "L" or decision == "l":
+        print("You selected the leftmost coin")
+        coinsArr.pop(0)
+
+    if decision == "R" or decision == "r":
+        print("You selected the rightmost coin")
+        coinsArr.pop()
+
+    print(ArrtoStr(coinsArr))
+
+    return len(coinsArr) > 0
+
+
+flag = True
+
+while flag:
+    flag = playing()
